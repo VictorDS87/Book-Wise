fe13eafa0531ae69f444330ba5a2d7ee49095a22
Victor Donizete <victordonizete65@gmail.com>
2023-05-12 22:23:53 -0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 import { GlobalStyles } from "./styles/global";
 
+// import { BookwiseProvider } from "./context/bookwiseContext";
 export function App() {
   return ( 
     <>
-        <GlobalStyles />
-        <BrowserRouter>
-            <Router />
-        </BrowserRouter>
+        <GlobalStyles />     
+          <BrowserRouter>
+              <Router />
+          </BrowserRouter>
     </>
   )
 }
